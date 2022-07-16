39a9d0042c2aa3d937ebbee744f33dbad7d2829a
Piturnah <peterhebden6@gmail.com>
2022-07-16 17:02:14 +0100

    Log framerate diagnostics

--- src/debug.rs
+++ src/debug.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{diagnostic::{LogDiagnosticsPlugin, FrameTimeDiagnosticsPlugin}, prelude::*};
 use bevy_inspector_egui::{RegisterInspectable, WorldInspectorPlugin};
 use bevy_rapier2d::prelude::RapierDebugRenderPlugin;
 
@@ -10,6 +10,8 @@ impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
         if cfg!(debug_assertions) {
             app.add_plugin(RapierDebugRenderPlugin::default())
+                .add_plugin(LogDiagnosticsPlugin::default())
+                .add_plugin(FrameTimeDiagnosticsPlugin::default())
                 .add_plugin(WorldInspectorPlugin::new())
                 .register_inspectable::<Player>()
                 .register_inspectable::<GroundDetection>()
