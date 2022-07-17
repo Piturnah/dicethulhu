56d7259e1666beaf0c85c0c2f8aaf26603ea4941
Piturnah <peterhebden6@gmail.com>
2022-07-17 18:36:31 +0100

    Invuln flashing

--- src/health.rs
+++ src/health.rs
@@ -14,7 +14,17 @@ pub struct Health {
 }
 #[derive(Component, Debug)]
 pub struct Invuln {
-    timer: Timer,
+    duration: Timer,
+    flash_period: Timer,
+}
+
+impl Default for Invuln {
+    fn default() -> Self {
+        Self {
+            duration: Timer::from_seconds(1.0, false),
+            flash_period: Timer::new(Duration::from_millis(200), true),
+        }
+    }
 }
 
 impl Plugin for HealthPlugin {
@@ -29,9 +39,19 @@ fn invuln(
     time: Res<Time>,
 ) {
     for (id, mut sprite, mut invuln) in query.iter_mut() {
-        sprite.color.set_a(0.1);
-        invuln.timer.tick(time.delta());
-        if invuln.timer.just_finished() {
+        invuln.duration.tick(time.delta());
+        invuln.flash_period.tick(time.delta());
+
+	// This is broken but ran out of time in jam
+        if invuln.flash_period.just_finished() {
+	    let alpha = sprite.color.a();
+	    sprite.color.set_a(match alpha {
+		0.1 => 0.0,
+		0.0 | 1.0 => 0.1,
+		_ => 1.0,
+	    });
+	}
+        if invuln.duration.just_finished() {
             sprite.color.set_a(1.0);
             commands.entity(id).remove::<Invuln>();
         }
@@ -44,9 +64,18 @@ fn damaged(
     player_query: Query<Entity, With<Player>>,
 ) {
     for (id, mut health) in query.iter_mut() {
-        commands.entity(id).remove::<Damaged>().insert(Invuln {
-            timer: Timer::new(Duration::from_millis(200), false),
-        });
+        commands.entity(id).remove::<Damaged>();
+
+        // Very hacky, but running out of time
+        if player_query.get(id).is_ok() {
+            commands.entity(id).insert(Invuln::default());
+        } else {
+            commands.entity(id).insert(Invuln {
+                duration: Timer::new(Duration::from_millis(200), false),
+                flash_period: Timer::new(Duration::from_millis(40), false),
+            });
+        }
+
         health.health -= 1;
         if health.health == 0 {
             commands.entity(id).despawn_recursive();
